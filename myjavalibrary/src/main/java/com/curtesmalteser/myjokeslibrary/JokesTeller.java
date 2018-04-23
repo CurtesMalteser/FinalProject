bbddb8133dfd083d1388e48926a94e18f8be6ece
CurtesMalteser <antonio_bastiao@hotmail.com>
2018-04-24 01:21:12 +0200

    Step 1 - Create a Java Library

---
+++ myjavalibrary/src/main/java/com/curtesmalteser/myjokeslibrary/JokesTeller.java
@@ -0,0 +1,18 @@
+package com.curtesmalteser.myjokeslibrary;
+
+public class JokesTeller {
+
+    private String myData;
+
+    public String getData() {
+        return myData;
+    }
+
+    public void setData(String data) {
+        myData = data;
+    }
+
+    public String getJoke() {
+        return "This is an awesome joke! Ah! Ah! Ah!... Wait! What is the joke BTW?";
+    }
+}
