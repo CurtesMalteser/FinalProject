2bcd35eb947b401e7226462238d0eb67db9dbf0c
CurtesMalteser <antonio_bastiao@hotmail.com>
2018-04-24 01:33:27 +0200

    Step 2 - Create an Android Library

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,5 +1,6 @@
 package com.udacity.gradle.builditbigger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -7,7 +8,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.curtesmalteser.myandroidlibrary.JokesActivity;
 import com.curtesmalteser.myjokeslibrary.JokesTeller;
+
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -44,6 +48,10 @@ public class MainActivity extends AppCompatActivity {
     public void tellJoke(View view) {
         JokesTeller joke = new JokesTeller();
         Toast.makeText(this, joke.getJoke(), Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MainActivity.this, JokesActivity.class);
+        intent.putExtra(getString(R.string.string_intent_joke), joke.getJoke());
+        startActivity(intent);
+
     }
 
 
---
+++ myandroidlibrary/src/main/java/com/curtesmalteser/myandroidlibrary/JokesActivity.java
@@ -0,0 +1,21 @@
+package com.curtesmalteser.myandroidlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class JokesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jokes);
+
+        TextView tvJokes = findViewById(R.id.tvJokes);
+
+        if (getIntent().hasExtra(getString(R.string.string_intent_joke))) {
+            tvJokes.setText(getIntent().getStringExtra(getString(R.string.string_intent_joke)));
+        }
+
+    }
+}
